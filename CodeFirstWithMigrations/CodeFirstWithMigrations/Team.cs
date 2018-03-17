83a7e3a9c6cce2deb4543e31ef88c30b226f9656
lilithakobyan <lilithakobyan1994@mail.ru>
2018-03-17 20:12:50 +0400

    add

--- CodeFirstWithMigrations/CodeFirstWithMigrations/Team.cs
+++ CodeFirstWithMigrations/CodeFirstWithMigrations/Team.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Security.Principal;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CodeFirstWithMigrations
 {
